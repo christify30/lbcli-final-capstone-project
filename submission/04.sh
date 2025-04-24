ffc58824749a3974287b3e6654831615da7f36c2
christify30 <39292608+christify30@users.noreply.github.com>
2025-04-24 17:20:31 +0200

    Update 04.sh

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,9 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
-bitcoin-cli -signet decoderawtransaction "$(bitcoin-cli -signet getrawtransaction d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7)" | jq -r '.vin[0].witness[-1]'
+bitcoin-cli -signet getrawtransaction d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7 1 \
+  | jq -r '
+      .vin[0] 
+      | if has("txinwitness") and (.txinwitness | length > 1)
+        then .txinwitness[1]
+        else .scriptSig.asm | split(" ")[1]
+      end
+    '
