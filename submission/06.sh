e35a9452935fd9d210f1f9009e6e13e3be8382bf
christify30 <39292608+christify30@users.noreply.github.com>
2025-04-24 17:29:50 +0200

    Update 06.sh

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,7 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+for txid in $(bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 243821) | jq -r '.tx[]'); do
+    # Check if any input has nSequence < 0xFFFFFFFE (4294967294)
+    bitcoin-cli -signet getrawtransaction "$txid" true | \
+    jq -e '.vin[] | select(.sequence < 4294967294)' >/dev/null && \
+    echo "RBF-signaling txid: $txid" && break
+done
