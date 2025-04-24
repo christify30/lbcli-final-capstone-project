cd4e2abe5767ec351616494f64bc2da35355f3f2
christify30 <39292608+christify30@users.noreply.github.com>
2025-04-24 17:36:14 +0200

    Update 08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,11 @@
 # what block height was this tx mined ?
 # 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+
+CLI="bitcoin-cli -signet"
+TXID="49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb"
+
+BLOCKHASH=$($CLI getrawtransaction "$TXID" 1 \
+  | jq -r .blockhash)
+
+$CLI getblock "$BLOCKHASH" 1 \
+  | jq -r .height
