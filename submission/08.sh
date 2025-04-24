e2ae299ed6412d2f49e7b848887a63a4fbf13aca
Ifeanyichukwu Njoku <tech@circuapp.com>
2025-04-24 22:24:39 +0200

    rewrite submision

--- submission/08.sh
+++ submission/08.sh
@@ -1,6 +1,5 @@
 # what block height was this tx mined ?
 # 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
-
 CLI="bitcoin-cli -signet"
 TXID="49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb"
 
