a28194aaa948e6e5582b912ab4a0a22c22c47f9a
christify30 <39292608+christify30@users.noreply.github.com>
2025-04-24 17:04:31 +0200

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,6 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+coinbase_txid=$(bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216128) | jq -r '.tx[0]')
+
+bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216351) | jq -r '.tx[]' | while read txid; do
+  bitcoin-cli -signet getrawtransaction "$txid" true | jq -e --arg cb "$coinbase_txid" '.vin[] | select(.txid == $cb)' >/dev/null && echo "$txid" && break
+done
